4672e51f159229eca3d7cc86fa4b667f2009b4e2
otaviootavio <otaviovacari@usp.br>
2024-05-20 00:54:11 -0300

    it works

--- my-hls-app/src/components/VideoPlayer.tsx
+++ my-hls-app/src/components/VideoPlayer.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import useHls from "@/hooks/useHls";
 
 const VideoPlayer: React.FC = () => {
-  const videoRef = useHls("http://localhost:3000/api/playlist");
+  const [playlistUrl, setPlaylistUrl] = useState("");
+  const videoRef = useHls(playlistUrl);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setPlaylistUrl(`${window.location.origin}/api/playlist`);
+    }
+  }, []);
 
   return (
     <div className="flex items-center bg-gray-900">
-      <video
-        ref={videoRef}
-        controls
-        className="w-full max-w-2xl border-4 border-gray-700"
-      ></video>
+      {playlistUrl && (
+        <video
+          ref={videoRef}
+          controls
+          className="w-full max-w-2xl border-4 border-gray-700"
+        ></video>
+      )}
     </div>
   );
 };
--- my-hls-app/src/hooks/useHls.tsx
+++ my-hls-app/src/hooks/useHls.tsx
@@ -1,13 +1,17 @@
-// src/hooks/useHls.ts
-
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Hls from "hls.js";
 import { useHashChain } from "@/context/HashChainContext";
 
 const useHls = (src: string) => {
-  const { hashChain, popHash } = useHashChain();
+  const { hashChain } = useHashChain();
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const hlsRef = useRef<Hls | null>(null);
+  const [localHashChain, setLocalHashChain] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Initialize the local hash chain when the component mounts
+    setLocalHashChain([...hashChain]);
+  }, [hashChain]);
 
   useEffect(() => {
     const video = videoRef.current;
@@ -17,18 +21,20 @@ const useHls = (src: string) => {
     if (Hls.isSupported()) {
       const hls = new Hls({
         xhrSetup: (xhr) => {
-          console.log(`hashChainRef: ${JSON.stringify(hashChain)}`);
-          if (hashChain.length === 0) {
-            console.error("Hash chain is empty");
+          if (localHashChain.length === 0) {
+            console.error("Local hash chain is empty");
             return;
           }
-          const currentHash = popHash();
-          console.log(
-            `Sending Payword-Header: ${currentHash}:${hashChain.length + 1}`
-          );
+          const currentHash = localHashChain.pop();
+          setLocalHashChain([...localHashChain]);
+          // console.log(
+          //   `Sending Payword-Header: ${currentHash}:${
+          //     localHashChain.length + 1
+          //   }`
+          // );
           xhr.setRequestHeader(
             "Payword-Header",
-            `${currentHash}:${hashChain.length + 1}`
+            `${currentHash}:${localHashChain.length + 1}`
           );
         },
       });
@@ -38,11 +44,11 @@ const useHls = (src: string) => {
       hls.loadSource(src);
       hls.attachMedia(video);
 
-      hls.on(Hls.Events.MANIFEST_PARSED, () => {
-        video
-          .play()
-          .catch((error) => console.error("Video play failed", error));
-      });
+      // hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      //   video
+      //     .play()
+      //     .catch((error) => console.error("Video play failed", error));
+      // });
 
       hls.on(Hls.Events.ERROR, (event, data) => {
         if (data.fatal) {
@@ -64,13 +70,13 @@ const useHls = (src: string) => {
       });
     } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
       video.src = src;
-      video.addEventListener("canplay", () => {
-        video
-          .play()
-          .catch((error) => console.error("Video play failed", error));
-      });
+      // video.addEventListener("canplay", () => {
+      //   video
+      //     .play()
+      //     .catch((error) => console.error("Video play failed", error));
+      // });
     }
-  }, [src, hashChain[0]]);
+  }, [src, localHashChain[0]]);
 
   return videoRef;
 };
