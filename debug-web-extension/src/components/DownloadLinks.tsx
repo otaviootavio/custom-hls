65084e975e30a8d5c5449b2f908532103eb52369
otavio <otaviovacari@usp.br>
2025-02-03 17:29:50 -0300

    remove dummy fetch button from DownloadLinks component

--- debug-web-extension/src/components/DownloadLinks.tsx
+++ debug-web-extension/src/components/DownloadLinks.tsx
@@ -21,18 +21,6 @@ export const DownloadLinks = () => {
           Download for Firefox
         </a>
       </Button>
-      <Button asChild size="sm" variant={"outline"}>
-        <p
-          onClick={async () => {
-            const data = await fetch(
-              "https://jsonplaceholder.typicode.com/todos/1"
-            );
-            console.log(await data.json());
-          }}
-        >
-          Dummy fetch
-        </p>
-      </Button>
     </div>
   );
 };
