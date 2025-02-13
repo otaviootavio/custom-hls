16c427c36b26eaac8d18e20bf2143e3abd75391a
otavio <otaviovacari@usp.br>
2025-02-12 22:05:47 -0300

    chore: remove unused configuration and ignore files across multiple projects

--- eth-payword/frontend/src/utils/scrollBehavior.ts
+++
@@ -1,9 +0,0 @@
-export function initializeScrollBehavior() {
-  if ("scrollRestoration" in history) {
-    history.scrollRestoration = "manual";
-  }
-
-  window.onload = function () {
-    window.scrollTo(0, 0);
-  };
-}
--- eth-payword/frontend/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_CONTRACT_ADDRESS: `0x${string}`;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
